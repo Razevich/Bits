e1e85115dca2b35e1d9cc49086ee5b92d916dfac
Razevich <razevich.c@gmail.com>
2016-02-17 11:18:45 -0600

    add pages index and about section

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,9 @@
+class PagesController < ApplicationController
+
+  def index
+  end
+
+  def show
+    # add a about section for the website
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  root to: "pages#index"
+
+  get "about", to: "pages#show" #about page for website
+
+
   devise_for :students, controllers: { sessions: "students/sessions", registrations: "students/registrations" }
   devise_for :non_profits, controllers: { sessions: "non_profits/sessions",registrations: "non_profits/registrations" }
 
