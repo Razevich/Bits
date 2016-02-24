9e4df65c211bf520be797c22c29037139b837614
Michael Jasinski <majasinski8@gmail.com>
2016-02-23 22:25:47 -0600

    Displaying all project for non profit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,7 @@
 class ProjectsController < ActionController::Base
 
 def index
+  @projects = Project.all
 end
 
 
@@ -11,12 +12,18 @@ end
 
 def create
   non_profit = current_non_profit
-  @project = Project.create(non_profit_id: non_profit.id, name: params[:name], github_link: [:github_link], description: [:description] )
+  @project = Project.new(project_params)
+  if @project.save
   redirect_to non_profit_projects_path
+  end
 end
 
 def show
 end
 
+def project_params
+   params.require(:project).permit(current_non_profit.id, :name, :github_link, :description)
+end
 
 end
+# non_profit_id: non_profit.id, name: [:name], github_link: [:github_link], description: [:description] )
