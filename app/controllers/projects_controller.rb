3fbdbcd88bd05a3b95d7c655856eb83a4beb1532
Michael Jasinski <majasinski8@gmail.com>
2016-02-23 21:01:56 -0600

    New projects store in database

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,7 +11,8 @@ end
 
 def create
   non_profit = current_non_profit
-  @project = Project.create(non_profit: current_non_profit.id, name: params[:name], github_link: [:github_link], description: [:description] )
+  @project = Project.create(non_profit_id: non_profit.id, name: params[:name], github_link: [:github_link], description: [:description] )
+  redirect_to non_profit_projects_path
 end
 
 def show
